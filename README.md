d7369fb4eeac60dc80773f0d0c6fa347b91f69f6
Nelson Choque <nelson.choque@paku.pe>
2025-11-21 16:08:43 -0500

    feat: :art: remove firebase crashlytics
    se removio crashlytics y se desativo impeller en el manifest

--- README.md
+++ README.md
@@ -218,11 +218,11 @@ flutter pub run flutter_native_splash:create --path=flutter_native_splash_scroll
 ```
 
 ```bash
-flutter build apk --release --flavor scrolltvMobile --dart-define=IS_TV=false --dart-define=VARIANT=scrolltv --no-enable-impeller
+flutter build apk --release --flavor scrolltvMobile --dart-define=IS_TV=false --dart-define=VARIANT=scrolltv
 ```
 
 ```bash
-flutter build apk --release --flavor scrolltvTv --dart-define=IS_TV=true --dart-define=VARIANT=scrolltv --target-platform android-arm,android-arm64,android-x64 --no-enable-impeller
+flutter build apk --release --flavor scrolltvTv --dart-define=IS_TV=true --dart-define=VARIANT=scrolltv --target-platform android-arm,android-arm64,android-x64
 ```
 
 subir al drive de releases
@@ -242,12 +242,12 @@ flutter pub run flutter_native_splash:create --path=flutter_native_splash_movie_
 ```
 
 ```bash
-flutter build apk --release --flavor moviehomeMobile --dart-define=IS_TV=false --dart-define=VARIANT=moviehome --no-enable-impeller
+flutter build apk --release --flavor moviehomeMobile --dart-define=IS_TV=false --dart-define=VARIANT=moviehome
 
 ```
 
 ```bash
-flutter build apk --release --flavor moviehomeTv --dart-define=IS_TV=true --dart-define=VARIANT=moviehome --target-platform android-arm,android-arm64,android-x64 --no-enable-impeller
+flutter build apk --release --flavor moviehomeTv --dart-define=IS_TV=true --dart-define=VARIANT=moviehome --target-platform android-arm,android-arm64,android-x64
 
 
 ```
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
-import 'dart:async';
-
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 import 'package:scrolltv_frontend_mobile_flutter/app/di.dart';
 import 'package:scrolltv_frontend_mobile_flutter/app/my_app.dart';
 import 'package:scrolltv_frontend_mobile_flutter/domain/repositories/user_repository.dart';
-import 'package:scrolltv_frontend_mobile_flutter/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -35,21 +30,22 @@ void main() async {
       DeviceOrientation.portraitDown,
     ]);
   }
+  runApp(MyApp(logUser: await userRepo.isUserLogged()));
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
 
-  // 🔥 Configura Crashlytics
-  FlutterError.onError = (FlutterErrorDetails details) async {
-    FlutterError.presentError(details);
-    FirebaseCrashlytics.instance.recordFlutterError(details); // reporta a Firebase
-  };
+  // // 🔥 Configura Crashlytics
+  // FlutterError.onError = (FlutterErrorDetails details) async {
+  //   FlutterError.presentError(details);
+  //   FirebaseCrashlytics.instance.recordFlutterError(details); // reporta a Firebase
+  // };
 
-  runZonedGuarded(() async {
-    runApp(MyApp(logUser: await userRepo.isUserLogged()));
-  }, (error, stack) async {
-    // Reporta errores no capturados
-    FirebaseCrashlytics.instance.recordError(error, stack);
-  });
+  // runZonedGuarded(() async {
+  //   runApp(MyApp(logUser: await userRepo.isUserLogged()));
+  // }, (error, stack) async {
+  //   // Reporta errores no capturados
+  //   FirebaseCrashlytics.instance.recordError(error, stack);
+  // });
 }
